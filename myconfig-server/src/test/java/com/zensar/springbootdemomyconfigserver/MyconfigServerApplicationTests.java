3fbee35454ba44748c3655714fc181d7da3d5a42
vishnuvardhan pokala <105355757+Vishnu1607112280@users.noreply.github.com>
2022-06-29 07:39:06 +0530

    Add files via upload

---
+++ myconfig-server/src/test/java/com/zensar/springbootdemomyconfigserver/MyconfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.springbootdemomyconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyconfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
